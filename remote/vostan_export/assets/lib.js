b54412c8d87670802f0547cb20d99e0830317c53
arman <arman@instigatedesign.com>
2014-12-20 13:58:32 +0400

    Commit sources with AGPL header files

--- remote/vostan_export/assets/lib.js
+++ remote/vostan_export/assets/lib.js
@@ -1,3 +1,28 @@
+
+/*
+This file is part of Vostan framework.
+
+Vostan is an open-source Information Management Platform that can be used to
+build GGG (web 3.0) web-sites and presentations using mind-map based
+human-machine-interaction paradigm and information management.
+
+Project home page [http://ggg.vostan.net]
+Copyright (c) 2011-2014 Instigate Mobile cjsc, http://ggg.instigatemobile.com 
+
+Vostan is a free software: you can redistribute it and/or modify it under the
+terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 (function() {
   var VostanController = function(config) {
     var _lang = config.defaultLang;
